839dc97ea0727d81a1cf9d498635252f28162e57
Dmitry Weiner <dmitry.weiner@gmail.com>
2014-03-16 21:02:25 +0700

    Fixed many bugs

--- dla_tree.ino
+++ dla_tree.ino
@@ -1,58 +1,99 @@
 #include <S65Display.h> 
 
+#define MAX_ARRAY_LENGTH 500
+
 S65Display lcd;
   
 //neighbor pixel directions
 byte nx[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 byte ny[] = {0, 1, 1, 1, 0, -1, -1, -1};
-byte screen[S65_WIDTH][S65_HEIGHT/8];
+byte screen [ MAX_ARRAY_LENGTH ];
+int currentArrayLength = 0;
+
+boolean isOnScreen(int x, int y) {
+  for (int i = 0; i < currentArrayLength; i++ ) {
+    if (screen[i*2] == x && screen[i*2+1] == y) {
+      return true;
+    }
+  }
+  return false;
+} 
+
+boolean setOnScreen(int x, int y) {
+  if (currentArrayLength >= (MAX_ARRAY_LENGTH/2)) {
+    return false;
+  }
+  if (!isOnScreen(x, y)){
+    screen[currentArrayLength*2]   = x;
+    screen[currentArrayLength*2+1] = y;
+    currentArrayLength++;
+    Serial.println(currentArrayLength);
+  }
+  return true;
+}
+
 void setup() 
 { 
   lcd.init(2); 
   lcd.clear(0); 
   randomSeed(analogRead(0));
+  Serial.begin(9600);
 } 
   
 
 void loop(){ 
     byte x, y;
-    int steps;
-    bool flag;
-    byte a;
-    //# start random walk
-    x = S65_WIDTH / 2;
-    y = S65_HEIGHT / 2;
-    steps = 0;
-    flag = false;
-    while (true) {
-        if (screen[x][y] == 0) {
-          //check the neighbors
-          for (byte k=0; k<7; k++){
-            byte xn, yn;
-            xn = x + nx[k];
-            yn = y + ny[k];
-            if (xn < 0 || xn > (S65_WIDTH - 1) || yn < 0 || yn > (S65_HEIGHT - 1)){
-              lcd.drawPixel(x, y, RGB(255, 255, 255));
-              screen[x][y] = 255;
-              flag = true;
-              break;
-            }
-            if (screen[xn][yn] > 0){
-              screen[x][y] = 255;
-              flag = true;
-              break;
-            }
-          }
-        }
-        if (flag == true){
+    byte xc, yc;
+    byte xn, yn;
+    float rm = 1.0;
+    int maxIt = 25;
+    int rmax;
+    float a;
+    
+    xc = (S65_WIDTH - 1) / 2;
+    yc = (S65_HEIGHT - 1) / 2;
+    rmax = min(xc, yc) - 1;
+    lcd.drawPixel(xc, yc, RGB(255, 255, 255));
+    setOnScreen(xc, yc);
+    
+    while (rm < rmax) {
+      boolean flag = false;
+      a = 3.14159265 * 2 * random(0, 100)/100;
+      x = xc + rm * cos(a);
+      y = yc + rm * sin(a);
+      for (int i = 0; i < maxIt; i++){
+          byte rand = random(0, 8);
+          x = x + nx[rand];
+          y = y + ny[rand];
+          if (x < 0 || x > (S65_WIDTH - 1) || y < 0 || y > (S65_HEIGHT - 1))
             break;
+          if (!isOnScreen(x, y)){
+              //check the neighbors
+              for (byte k = 0; k < 8; k++){
+                  xn = x + nx[k];
+                  yn = y + ny[k];
+                  if (isOnScreen(xn, yn)) {
+                      lcd.drawPixel(x, y, RGB(255, 255, 255));
+                      setOnScreen(x, y);
+                      float r;
+                      r = sqrt((x - xc) * (x - xc) + (y - yc) * (y - yc));
+                      if (r > rm);
+                          rm = r;
+                      flag = true;
+                      break;
+                  }
+              }
+          }
+          if (flag)
+              break;
         }
-        a = random(0, 7);
-        x = x + nx[a];
-        y = y + ny[a];
-        steps += 1;
-    }
-    if (steps < 2) {
-      lcd.clear(0); 
+        Serial.print("x = ");
+        Serial.print(x);
+        Serial.print(" y = ");
+        Serial.print(y);
+        Serial.print(" a = ");
+        Serial.print(a);
+        Serial.print(" rm = ");
+        Serial.println(rm);
     }
 }
